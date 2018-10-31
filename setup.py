e2ca86b0cb490be8377f951f955470e89f3069f6
Michał Tyszkiewicz <michal.tyszkiewicz@gmail.com>
2018-10-31 12:31:18 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='torch-dimcheck',
+    version='0.0.1',
+    description='Dimensionality annotations for tensor parameters and return values',
+    packages=['src'],
+    author='Michał Tyszkiewicz',
+    author_email='michal.tyszkiewicz@gmail.com',
+)
