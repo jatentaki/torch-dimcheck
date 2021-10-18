860644dc9afd50d387f7fbb04ac467ee8c981d22
Michał Tyszkiewicz <michal.tyszkiewicz@gmail.com>
2021-10-18 17:09:35 +0200

    First version of v2

---
+++ v2.py
@@ -0,0 +1,152 @@
+import inspect
+import torch
+from dataclasses import dataclass
+from typing import Union, Optional, Tuple, Tuple, Dict, Set, List
+
+@dataclass(unsafe_hash=True)
+class Token:
+    label: Union[int, str]
+    
+    @classmethod
+    def from_str(cls, string: str) -> 'Token':
+        try:
+            return cls(int(string))
+        except ValueError:
+            return cls(string)
+
+    @classmethod
+    def tokenize(cls, annotation: str) -> Tuple['Token']:
+        return tuple(cls.from_str(s) for s in annotation.split())
+
+ParseDict = Dict[Union[str, int], int]
+
+@dataclass(unsafe_hash=True)
+class A:
+    raw: str
+    labels: Tuple[Token]
+    
+    def __call__(self):
+        # this is just to fake the callable interface for typing
+        return NotImplemented()
+    
+    @classmethod
+    def __class_getitem__(cls, annotation: str) -> 'A':
+        return A(annotation, Token.tokenize(annotation))
+
+    def parse_shape(self, shape: Tuple[int]) -> ParseDict:
+        parse_dict: ParseDict = {}
+            
+        if len(shape) != len(self.labels):
+            raise TypeError(f'Got shape of length {len(shape)} with an '
+                            f'annotation of length {(len(self.labels))} '
+                            f'({self.raw}) vs ({shape}).')
+        
+        for dim, label in zip(shape, self.labels):
+            parse_dict[label.label] = dim
+        
+        return parse_dict
+    
+    def render(self, parse_dict: ParseDict) -> str:
+        tokens = []
+        for label in self.labels:
+            value = parse_dict.get(label.label, '?')
+            tokens.append(f'{label.label}={value}')
+        return ' '.join(tokens)
+
+@dataclass
+class ConstError:
+    tensor_name: str
+    expected: int
+    found: int
+    
+    def __str__(self) -> str:
+        return f'{self.tensor_name}: {self.expected} != {self.found}'
+
+@dataclass
+class Inconsistency:
+    label: str
+    values: Set[int]
+    
+    def __str__(self) -> str:
+        return f'Inconsistency: {self.label} = {list(self.values)}'
+
+class ShapeError(TypeError):
+    def __init__(self, issues: List[Union[ConstError, Inconsistency]], context: List[str]):
+        self.issues = issues
+        self.context = context
+    
+    def __str__(self) -> str:
+        issues = '\n\t'.join(str(i) for i in self.issues)
+        context = '\n\t'.join(self.context)
+        return f'Issues:\n\t{issues}\nContext:\n\t{context}'
+
+def check_consistency(parses: Dict[str, ParseDict]) -> Optional[ShapeError]:
+    issues = []
+    
+    bindings: Dict[str, Set[int]] = {}
+        
+    for tensor_name, tensor_parses in parses.items():
+        for label, value in tensor_parses.items():
+            if isinstance(label, int):
+                if label != value:
+                    issues.append(ConstError(tensor_name=tensor_name, expected=label, found=value))
+            elif isinstance(label, str):
+                bindings.setdefault(label, set()).add(value)
+            else:
+                raise AssertionError('Unreachable')
+    
+    for label, values in bindings.items():
+        if len(values) > 1:
+            issues.append(Inconsistency(label=label, values=values))
+        else:
+            assert len(values) == 1
+    
+    if not issues:
+        return None
+    
+    return ShapeError(issues, context=[])
+    
+def _zip_args_and_labels(args, signature):
+    for arg, parameter in zip(args, signature.parameters.values()):
+        yield parameter.name, arg, parameter.annotation
+
+def _is_optional_annotation(type_) -> bool:
+    return hasattr(type_, '__origin__') \
+        and type_.__origin__ == Union \
+        and len(type_.__args__) == 2 \
+        and type_.__args__[1] == type(None) \
+        and isinstance(type_.__args__[0], A)
+
+def dimchecked(func):
+    signature = inspect.signature(func)
+    
+    def wrapped(*args, **kwargs):
+        parses: Dict[str, ParseDict] = {}
+        annotations: Dict[str, A] = {}
+        
+        for name, value, annotation in _zip_args_and_labels(args, signature):
+            if _is_optional_annotation(annotation):
+                if value is None:
+                    continue
+                else:
+                    annotation = annotation.__args__[0]
+
+            if not isinstance(annotation, A):
+                continue
+            
+            if not isinstance(value, torch.Tensor):
+                raise TypeError(f'Expected {name} to be a torch.Tensor, found {type(value)}.')
+            
+            parses[name] = annotation.parse_shape(value.shape)
+            annotations[name] = annotation
+        
+        maybe_error = check_consistency(parses)
+        if maybe_error is not None:
+            for name, annotation in annotations.items():
+                rendered = annotation.render(parses[name])
+                maybe_error.context.append(f'{name}: {rendered}')
+            raise maybe_error
+
+        result = func(*args, **kwargs)
+        return result
+    return wrapped
